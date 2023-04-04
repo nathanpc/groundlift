64251ea4d7f73d0fa1c7c252db83833caa83feed
Nathan Campos <nathan@innoveworkshop.com>
2023-04-04 10:39:00 +0100

    Proper handling of thread return and implemented mutexes in client module
    Basically ported the most important changes done to the server moduule
    to the client module.

--- cli/client.c
+++ cli/client.c
@@ -13,6 +13,7 @@
 
 /* Private variables. */
 static tcp_client_t *m_client;
+static pthread_mutex_t *m_client_mutex;
 static pthread_t *m_client_thread;
 
 /* Private methods. */
@@ -31,6 +32,10 @@ void *client_thread_func(void *args);
 bool gl_client_init(const char *addr, uint16_t port) {
 	/* Ensure we have everything in a known clean state. */
 	m_client_thread = (pthread_t *)malloc(sizeof(pthread_t));
+
+	/* Initialize our mutex. */
+	m_client_mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_init(m_client_mutex, NULL);
 
 	/* Get a server handle. */
 	m_client = tcp_client_new(addr, port);
@@ -51,6 +56,12 @@ void gl_client_free(void) {
 
 	/* Join the client thread. */
 	gl_client_thread_join();
+
+	/* Free our mutex. */
+	if (m_client_mutex) {
+		free(m_client_mutex);
+		m_client_mutex = NULL;
+	}
 }
 
 /**
@@ -81,12 +92,18 @@ bool gl_client_connect(void) {
  * @see gl_client_connect
  */
 bool gl_client_disconnect(void) {
+	tcp_err_t err;
+
 	/* Do we even need to shut it down? */
 	if (m_client == NULL)
 		return true;
 
 	/* Shut the connection down. */
-	return tcp_client_shutdown(m_client) == TCP_OK;
+	pthread_mutex_lock(m_client_mutex);
+	err = tcp_client_shutdown(m_client);
+	pthread_mutex_unlock(m_client_mutex);
+
+	return err == TCP_OK;
 }
 
 /**
@@ -95,19 +112,24 @@ bool gl_client_disconnect(void) {
  * @return TRUE if the operation was successful.
  */
 bool gl_client_thread_join(void) {
-	void *retval;
+	tcp_err_t *err;
+	bool success;
 
 	/* Do we even need to do something? */
 	if (m_client_thread == NULL)
 		return true;
 
 	/* Join the thread back into us. */
-	pthread_join(*m_client_thread, &retval);
+	pthread_join(*m_client_thread, (void **)&err);
 	free(m_client_thread);
 	m_client_thread = NULL;
 
+	/* Check for success and free our returned value. */
+	success = *err <= TCP_OK;
+	free(err);
+
 	/* Check if the thread join was successful. */
-	return retval == 0;
+	return success;
 }
 
 /**
@@ -121,13 +143,15 @@ tcp_client_t *gl_client_get(void) {
 
 /**
  * Client's thread function.
+ * @warning This function allocates memory that must be free'd by you.
  *
  * @param args No arguments should be passed.
  *
- * @return Returned value from the whole operation.
+ * @return Pointer to a tcp_err_t with the last error code. This pointer must be
+ *         free'd by you.
  */
 void *client_thread_func(void *args) {
-	tcp_err_t err;
+	tcp_err_t *err;
 	char *tmp;
 	char buf[100];
 	size_t len;
@@ -135,16 +159,19 @@ void *client_thread_func(void *args) {
 	/* Ignore the argument passed. */
 	(void)args;
 	tmp = NULL;
+	err = (tcp_err_t *)malloc(sizeof(tcp_err_t));
 
 	/* Get a client handle. */
 	if (m_client == NULL) {
 		fprintf(stderr, "server_thread_func: Client handle is NULL.\n");
-		return (void *)TCP_ERR_UNKNOWN;
+
+		*err = TCP_ERR_UNKNOWN;
+		return (void *)err;
 	}
 
 	/* Connect our client to a server. */
-	err = tcp_client_connect(m_client);
-	if (err)
+	*err = tcp_client_connect(m_client);
+	if (*err)
 		return (void *)err;
 
 	/* Print some information about the current state of the connection. */
@@ -153,7 +180,7 @@ void *client_thread_func(void *args) {
 		   ntohs(m_client->addr_in.sin_port));
 
 	/* Read incoming data until the connection is closed by the server. */
-	while ((err = tcp_client_recv(m_client, buf, 99, &len, false)) == TCP_OK) {
+	while ((*err = tcp_client_recv(m_client, buf, 99, &len, false)) == TCP_OK) {
 		/* Properly terminate the received string and print it. */
 		buf[len] = '\0';
 		printf("Data received from %s: \"%s\"\n", tmp, buf);
@@ -164,9 +191,10 @@ void *client_thread_func(void *args) {
 	}
 
 	/* Check if the connection was closed gracefully. */
-	if (err == TCP_EVT_CONN_CLOSED)
+	if (*err == TCP_EVT_CONN_CLOSED) {
 		printf("Connection closed by the server at %s:%u\n", tmp,
 			   ntohs(m_client->addr_in.sin_port));
+	}
 
 	/* Disconnect from the server and free up any resources. */
 	gl_client_disconnect();
@@ -175,10 +203,6 @@ void *client_thread_func(void *args) {
 	/* Ensure temp buffer is free'd. */
 	if (tmp)
 		free(tmp);
-
-	/* Ensure we don't end up with weird false errors. */
-	if (err < TCP_OK)
-		err = 0;
 
 	return (void *)err;
 }
