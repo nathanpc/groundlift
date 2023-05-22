1d1ad94ea1cfbeea904d8bd10e6bc76f76104e8a
Nathan Campos <nathan@innoveworkshop.com>
2023-05-22 13:02:55 +0100

    win32: Created a Peer model to represent a peer device on the network
    Given the fact that we'll have to be passing around data about the peers
    on the network, a model class of it will come in handy.

---
+++ win32/src/Models/Peer.h
@@ -0,0 +1,47 @@
+/**
+ * Peer.h
+ * GroundLift peer device on the network.
+ * 
+ * @author Nathan Campos <nathan@innoveworkshop.com>
+ */
+
+#ifndef _WINGL_MODELS_PEER_H
+#define _WINGL_MODELS_PEER_H
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include <string>
+#include <groundlift/client.h>
+
+namespace GroundLift {
+
+/**
+ * GroundLift peer device on the network.
+ */
+class Peer {
+private:
+	LPTSTR szDeviceType;
+	LPTSTR szHostname;
+	struct sockaddr *saAddress;
+	socklen_t lenAddress;
+
+public:
+	Peer(LPCTSTR szDeviceType, LPCTSTR szHostname,
+		 const struct sockaddr *saAddress, socklen_t lenAddress);
+	Peer() : Peer(NULL, NULL, NULL, 0) {};
+	Peer(LPCTSTR szDeviceType, LPCTSTR szHostname, LPCTSTR szIPAddress,
+		 USHORT usPort);
+	virtual ~Peer();
+
+	LPCTSTR DeviceType();
+	LPCTSTR Hostname();
+	const struct sockaddr *SocketAddress();
+	socklen_t SocketAddressLength();
+	LPTSTR IPAddress();
+};
+
+}
+
+#endif // _WINGL_MODELS_PEER_H
