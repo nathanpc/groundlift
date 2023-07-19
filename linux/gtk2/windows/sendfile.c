ce539cfe6cdaf24016e8ef1527134035f7b8ab74
Nathan Campos <nathan@innoveworkshop.com>
2023-07-19 13:17:39 +0100

    linux/gtk2: Populating the peer list in the Send File window
    The Send File window is now almost fully implemented, only missing the
    actual action of sending the file, since the peer list is now being
    populated by the discovery service and selecting a peer will populate
    the contents of the IP address entry field.

--- linux/gtk2/windows/sendfile.c
+++ linux/gtk2/windows/sendfile.c
@@ -7,10 +7,14 @@
 
 #include "sendfile.h"
 
+#include <groundlift/client.h>
 #include <groundlift/defaults.h>
+#include <groundlift/error.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "../comdlg.h"
 
 /* Define GObject type. */
 G_DEFINE_TYPE(SendFileWindow, sendfile_window, GTK_TYPE_WINDOW)
@@ -24,13 +28,22 @@ typedef enum {
 	N_COLUMNS
 } peer_list_cols_t;
 
+/* Private methods. */
+static void sendfile_window_finalize(GObject *gobject);
+void cancel_button_clicked(GtkWidget *widget, gpointer data);
+void tree_selection_changed(GtkTreeSelection *selection, gpointer data);
+void gl_event_peer_discovered(const gl_discovery_peer_t *peer, void *arg);
+
 /**
  * Send File window class initializer.
  *
  * @param klass Send File window class.
  */
 static void sendfile_window_class_init(SendFileWindowClass *klass) {
-	/* This space was intentionally left blank. */
+	GObjectClass *object_class = G_OBJECT_CLASS(klass);
+
+	/* Handle some standard object events. */
+	object_class->finalize = sendfile_window_finalize;
 }
 
 /**
@@ -47,10 +60,13 @@ static void sendfile_window_init(SendFileWindow *self) {
 	GtkWidget *entry;
 	GtkWidget *button;
 
-	GtkTreeStore *store;
 	GtkWidget *tree;
 	GtkTreeViewColumn *column;
 	GtkCellRenderer *renderer;
+	GtkTreeSelection *select;
+
+	/* Set some defaults. */
+	self->gl_discovery_client = NULL;
 
 	/* Create the root window. */
 	window = GTK_WIDGET(self);
@@ -71,6 +87,7 @@ static void sendfile_window_init(SendFileWindow *self) {
 	button = gtk_file_chooser_button_new("File to Send",
 										 GTK_FILE_CHOOSER_ACTION_OPEN);
 	gtk_box_pack_start(GTK_BOX(hbox), button, true, true, 0);
+	self->file_button = button;
 
 	/* IP address entry field. */
 	hbox = gtk_hbox_new(false, 1);
@@ -79,13 +96,14 @@ static void sendfile_window_init(SendFileWindow *self) {
 	gtk_box_pack_start(GTK_BOX(hbox), label, false, false, 0);
 	entry = gtk_entry_new();
 	gtk_box_pack_start(GTK_BOX(hbox), entry, true, true, 0);
+	self->ipaddr_entry = entry;
 
 	/* Peer list tree. */
-	store = gtk_tree_store_new(N_COLUMNS,
-							   G_TYPE_STRING,
-							   G_TYPE_STRING);
-	tree = gtk_tree_view_new_with_model(GTK_TREE_MODEL(store));
-	g_object_unref(G_OBJECT(store));
+	self->peer_list_store = gtk_list_store_new(N_COLUMNS,
+											   G_TYPE_STRING,
+											   G_TYPE_STRING);
+	tree = gtk_tree_view_new_with_model(GTK_TREE_MODEL(self->peer_list_store));
+	g_object_unref(G_OBJECT(self->peer_list_store));
 	renderer = gtk_cell_renderer_text_new();
 	column = gtk_tree_view_column_new_with_attributes("Hostname", renderer,
 													  "text", HOSTNAME_COLUMN,
@@ -96,20 +114,31 @@ static void sendfile_window_init(SendFileWindow *self) {
 													  "text", IPADDR_COLUMN,
 													  NULL);
 	gtk_tree_view_append_column(GTK_TREE_VIEW(tree), column);
+	select = gtk_tree_view_get_selection(GTK_TREE_VIEW(tree));
+	gtk_tree_selection_set_mode(select, GTK_SELECTION_SINGLE);
+	g_signal_connect(G_OBJECT(select), "changed",
+					 G_CALLBACK(tree_selection_changed), self);
 	gtk_box_pack_start(GTK_BOX(vbox), tree, true, true, 0);
+	self->peer_tree = tree;
 
 	/* Dialog buttons. */
 	hbox = gtk_hbutton_box_new();
 	gtk_button_box_set_spacing(GTK_BOX(hbox), 5);
 	gtk_button_box_set_layout(GTK_BUTTON_BOX(hbox), GTK_BUTTONBOX_END);
 	gtk_box_pack_start(GTK_BOX(vbox), hbox, false, false, 0);
-	button = gtk_button_new_with_label("Close");
+	button = gtk_button_new_from_stock(GTK_STOCK_CANCEL);
+	g_signal_connect(G_OBJECT(button), "clicked",
+					 G_CALLBACK(cancel_button_clicked), self);
 	gtk_box_pack_start(GTK_BOX(hbox), button, false, false, 0);
-	button = gtk_button_new_with_label("Send");
+	button = gtk_button_new_from_stock(GTK_STOCK_CONNECT);
+	gtk_button_set_label(GTK_BUTTON(button), "Send");
 	gtk_box_pack_start(GTK_BOX(hbox), button, false, false, 0);
 
 	/* Show window and its widgets. */
 	gtk_widget_show_all(window);
+
+	/* Discover the peers on the network and populate the peer list. */
+	sendfile_window_peers_discover(self);
 }
 
 /**
@@ -119,4 +148,211 @@ static void sendfile_window_init(SendFileWindow *self) {
  */
 GtkWidget *sendfile_window_new(void) {
 	return GTK_WIDGET(g_object_new(SENDFILE_TYPE_WINDOW, NULL));
+}
+
+/**
+ * Handle the object's destruction and frees up any resources.
+ *
+ * @param gobject Send File window object.
+ */
+static void sendfile_window_finalize(GObject *gobject) {
+	/* Free up our used resources. */
+	gl_client_discovery_free(SENDFILE_WINDOW(gobject)->gl_discovery_client);
+
+	/* Always chain up to the parent class. */
+	G_OBJECT_CLASS(sendfile_window_parent_class)->finalize(gobject);
+}
+
+/**
+ * Sends a discovery broadcast and populates the peers list with the responses.
+ *
+ * @warning This function will clear the peers list before populating it.
+ *
+ * @param window Send File Window object.
+ */
+void sendfile_window_peers_discover(SendFileWindow *window) {
+	iface_info_list_t *if_list;
+	tcp_err_t tcp_err;
+	gl_err_t *err;
+	int i;
+
+	/* Start with a clean slate. */
+	err = NULL;
+	sendfile_window_peers_clear(window);
+
+	/* Get the list of network interfaces. */
+	tcp_err = socket_iface_info_list(&if_list);
+	if (tcp_err) {
+		err = gl_error_new_errno(ERR_TYPE_TCP, (int8_t)tcp_err,
+			EMSG("Failed to get the list of network interfaces"));
+		goto cleanup;
+	}
+
+	/* Go through the network interfaces. */
+	for (i = 0; i < if_list->count; i++) {
+		iface_info_t *iface;
+
+		/* Get the network interface and search for peers on the network. */
+		iface = if_list->ifaces[i];
+		sendfile_window_peers_discover_with_addr(window, iface->brdaddr);
+	}
+
+cleanup:
+	/* Free our network interface list. */
+	socket_iface_info_list_free(if_list);
+
+	/* Check for errors. */
+	if (err)
+		dialog_error_gl(GTK_WINDOW(window), err);
+}
+
+/**
+ * Sends a discovery broadcast on a specific socket address and appends the
+ * responses to the peers list.
+ *
+ * @note This function appends peers to the list, meaning it won't clear it.
+ *
+ * @param window    Send File Window object.
+ * @param sock_addr Broadcast IP address to connect/bind to or NULL if the
+ *                  address should be INADDR_BROADCAST.
+ */
+void sendfile_window_peers_discover_with_addr(SendFileWindow *window,
+		const struct sockaddr *sock_addr) {
+	gl_err_t *err;
+
+	/* Construct the discovery client handle object. */
+	window->gl_discovery_client = gl_client_discovery_new();
+	if (window->gl_discovery_client == NULL) {
+		err = gl_error_new_errno(ERR_TYPE_GL, GL_ERR_UNKNOWN,
+			EMSG("Failed to construct the discovery client handle object"));
+		dialog_error_gl(GTK_WINDOW(window), err);
+		return;
+	}
+
+	/* Setup event handlers. */
+	gl_client_evt_discovery_peer_set(window->gl_discovery_client,
+									 gl_event_peer_discovered, (void *)window);
+
+	/* Setup discovery socket. */
+	if (sock_addr == NULL) {
+		/* Use the 255.255.255.255 IP address for broadcasting. */
+		err = gl_client_discovery_setup(window->gl_discovery_client,
+										INADDR_BROADCAST, UDPSERVER_PORT);
+	} else {
+		/* Use a specific broadcast IP address for a specific network. */
+		switch (sock_addr->sa_family) {
+			case AF_INET:
+				err = gl_client_discovery_setup(window->gl_discovery_client,
+					((const struct sockaddr_in *)sock_addr)->sin_addr.s_addr,
+					UDPSERVER_PORT);
+				break;
+			default:
+				err = gl_error_new(ERR_TYPE_GL, GL_ERR_UNKNOWN,
+					EMSG("Discovery service only works with IPv4"));
+				break;
+		}
+	}
+
+	/* Check for errors. */
+	if (err) {
+		dialog_error_gl(GTK_WINDOW(window), err);
+		goto cleanup;
+	}
+
+	/* Send discovery broadcast and listen to replies. */
+	err = gl_client_discover_peers(window->gl_discovery_client);
+	if (err) {
+		dialog_error_gl(GTK_WINDOW(window), err);
+		goto cleanup;
+	}
+
+	/* Wait for the discovery thread to return. */
+	err = gl_client_discovery_thread_join(window->gl_discovery_client);
+	if (err) {
+		dialog_error_gl(GTK_WINDOW(window), err);
+		goto cleanup;
+	}
+
+cleanup:
+	/* Clean things up. */
+	gl_client_discovery_free(window->gl_discovery_client);
+	window->gl_discovery_client = NULL;
+}
+
+/**
+ * Clears the contents of the peer list.
+ *
+ * @param window Send File Window object.
+ */
+void sendfile_window_peers_clear(SendFileWindow *window) {
+	gtk_list_store_clear(window->peer_list_store);
+}
+
+/**
+ * Appends a peer to the peer list.
+ *
+ * @param window Send File Window object.
+ * @param peer   Peer object to be appended to the list.
+ */
+void sendfile_window_peers_append(SendFileWindow *window,
+								  const gl_discovery_peer_t *peer) {
+	gtk_list_store_insert_with_values(window->peer_list_store, NULL, -1,
+		HOSTNAME_COLUMN, peer->name,
+		IPADDR_COLUMN, inet_ntoa(peer->sock->addr_in.sin_addr), -1);
+}
+
+/**
+ * Appends a peer to the peer list from specific hostname and IP address values.
+ *
+ * @param window   Send File Window object.
+ * @param hostname Peer's hostname.
+ * @param ipaddr   Peer's IP address.
+ */
+void sendfile_window_peers_append_with_values(SendFileWindow *window,
+											  const gchar *hostname,
+											  const gchar *ipaddr) {
+	gtk_list_store_insert_with_values(window->peer_list_store, NULL, -1,
+		HOSTNAME_COLUMN, hostname, IPADDR_COLUMN, ipaddr, -1);
+}
+
+/**
+ * Peer tree view selection changed event handler.
+ *
+ * @param selection Selected item in the peer list.
+ * @param data      Pointer to the window's widget.
+ */
+void tree_selection_changed(GtkTreeSelection *selection, gpointer data) {
+	GtkTreeIter iter;
+	GtkTreeModel *model;
+	gchar *ipaddr;
+
+	/* Get the selected item and populate the IP address entry field. */
+	if (gtk_tree_selection_get_selected(selection, &model, &iter)) {
+		gtk_tree_model_get(model, &iter, IPADDR_COLUMN, &ipaddr, -1);
+		gtk_entry_set_text(GTK_ENTRY(SENDFILE_WINDOW(data)->ipaddr_entry),
+						   ipaddr);
+
+		g_free(ipaddr);
+	}
+}
+
+/**
+ * Cancel button clicked event handler.
+ *
+ * @param widget Widget that triggered this event handler.
+ * @param data   Pointer to the window's widget.
+ */
+void cancel_button_clicked(GtkWidget *widget, gpointer data) {
+	(void)widget;
+	gtk_widget_destroy(GTK_WIDGET(data));
+}
+
+/**
+ * Handles the peer discovered event.
+ *
+ * @param peer Discovered peer object.
+ * @param arg  Send File Window object.
+ */
+void gl_event_peer_discovered(const gl_discovery_peer_t *peer, void *arg) {
+	sendfile_window_peers_append(SENDFILE_WINDOW(arg), peer);
 }
--- linux/gtk2/windows/sendfile.h
+++ linux/gtk2/windows/sendfile.h
@@ -8,6 +8,7 @@
 #ifndef _GL_GTK_WINDOWS_SENDFILE_H
 #define _GL_GTK_WINDOWS_SENDFILE_H
 
+#include <groundlift/client.h>
 #include <gtk/gtk.h>
 
 #ifdef __cplusplus
@@ -31,14 +32,34 @@ typedef struct _SendFileWindowClass SendFileWindowClass;
 
 struct _SendFileWindow {
 	GtkWindow parent_instance;
+
+	discovery_client_t *gl_discovery_client;
+
+	GtkWidget *ipaddr_entry;
+	GtkWidget *file_button;
+
+	GtkListStore *peer_list_store;
+	GtkWidget *peer_tree;
 };
 
 struct _SendFileWindowClass {
 	GtkWindowClass parent_class;
 };
 
+/* Constructor */
 GType sendfile_window_get_type(void);
 GtkWidget *sendfile_window_new(void);
+
+/* Peer list operations. */
+void sendfile_window_peers_discover(SendFileWindow *window);
+void sendfile_window_peers_discover_with_addr(SendFileWindow *window,
+											  const struct sockaddr *sock_addr);
+void sendfile_window_peers_clear(SendFileWindow *window);
+void sendfile_window_peers_append(SendFileWindow *window,
+								  const gl_discovery_peer_t *peer);
+void sendfile_window_peers_append_with_values(SendFileWindow *window,
+											  const gchar *hostname,
+											  const gchar *ipaddr);
 
 #ifdef __cplusplus
 }
